9be6048cda17fdba7b1cbabc95332af084f75d5f
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-11-02 14:09:54 -0700

    added mock data

---
+++ src/mockMovies.js
@@ -0,0 +1,37 @@
+const mockMovies = [
+    {
+        id: 1,
+        title: 'born again',
+        genre: 'action',
+        image: 'https://images.unsplash.com/photo-1627465688839-f088954a40a0?auto=format&fit=crop&q=80&w=982&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        trailer: 'https://www.youtube.com/watch?v=V-yt7-DJZyY&ab_channel=Soundridemusic-NoCopyrightMusic',
+        rating: 4,
+        description: 'Ex Marine who turned his life to the lord has now been called back into action',
+        platform: 'https://www.youtube.com/',
+        user_id: 1
+    },
+    {
+        id: 2,
+        title: 'Fed Up',
+        genre: 'comedy',
+        image: 'https://images.unsplash.com/photo-1611956425642-d5a8169abd63?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29tZWR5fGVufDB8fDB8fHww',
+        trailer: 'https://www.youtube.com/watch?v=1zNxsEyK4E0&ab_channel=BrokenC-Music',
+        rating: 4,
+        description: "He's fed up and here's his story ",
+        platform: 'https://www.youtube.com/',
+        user_id: 2
+    },
+    {
+        id: 3,
+        title: 'Whispers Of Decay',
+        genre: 'Horror',
+        image: 'https://unsplash.com/photos/person-behind-fog-glass-ZC0EbdLC8G0',
+        trailer: 'https://www.youtube.com/watch?v=E_Ty9L3P3Ls&list=PLY4oAfouVgHI_DzPRn4ixYibkM03x-QIA&ab_channel=ElysiumAudioLabs',
+        rating: 2,
+        description: "Masked man hunting people",
+        platform: 'https://www.youtube.com/',
+        user_id: 3
+    },
+]
+
+export default mockMovies
---
+++ src/mockUsers.js
@@ -0,0 +1,14 @@
+let mockUsers = [
+  {
+    id: 1,
+    username: "john",
+    email: "test1@example.com"
+  },
+  {
+    id: 2,
+    username: "david",
+    email: "test2@example.com"
+  }
+]
+
+export default mockUsers
