af8326e59f52f69c277613d265ee4e8ad50daa7d
Adrian Ramirez <adrian.ramirez45679@gmail.com>
2023-11-07 13:10:13 -0800

    fixing merge conflict

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
+
 import React, { useState } from "react";
-import Home from "./pages/Home"
-import MovieIndex from "./pages/MovieIndex"
-import MovieShow from "./pages/MovieShow"
-import MovieEdit from "./pages/MovieEdit"
-import NotFound from './pages/NotFound'
-import AboutUs from "./pages/AboutUs"
-import SignInSignUp from "./pages/SignUpSignIn"
-import { Routes, Route } from "react-router-dom"
-import SignUp from "./pages/SignUp";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import MovieIndex from "./pages/MovieIndex";
+import MovieShow from "./pages/MovieShow";
+import MovieEdit from "./pages/MovieEdit";
+import NotFound from "./pages/NotFound";
+import AboutUs from "./pages/AboutUs";
+import SignInSignUp from "./pages/SignUpSignIn";
+import SignUp from "./pages/SignUp"; 
 import SignIn from "./pages/SignIn";
-import mockMovies from "./mockMovies";
-import mockUsers from "./mockUsers.js";
-import MovieNew from "./pages/MovieNew.js"
-import UserMovies from "./pages/UserMovies.js"
+import MovieNew from "./pages/MovieNew.js";
+import UserMovies from "./pages/UserMovies.js";
 
 const App = () => {
   const [movies, setMovies] = useState(mockMovies)
   const [currentUser, setCurrentUser] = useState(mockUsers[0])
  
   return(
-    <body>
+    <>
       <Routes>
         <Route path="/" element={<SignInSignUp />}/>
         <Route path="/home" element={<Home />}/>
-        <Route path="/movieindex" element={<MovieIndex movies={movies} currentUser={currentUser}/>}/>
+        <Route path="/movieindex" element={<MovieIndex />}/>
         <Route path="/movienew" element={<MovieNew />}/>
         <Route path="/usermovies" element={<UserMovies />}/>
-        <Route path="/movieshow/:id" element={<MovieShow movies={movies} currentUser={currentUser}/>}/>
+        <Route path="/movieshow" element={<MovieShow />}/>
         <Route path="/movieedit" element={<MovieEdit />}/>
         <Route path="/aboutus" element={<AboutUs />}/>
         <Route path="/signup" element={<SignUp />}/>
@@ -34,9 +33,6 @@ const App = () => {
         <Route path="*" element={<NotFound />}/>
       </Routes>
   
-    </body>
+    </>
   )
 
-}
-
-export default App
